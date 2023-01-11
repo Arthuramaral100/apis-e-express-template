539fb5437e7b94e737e89aba2d9b72bf210e0b8e
Arthuramaral100 <arthuramaral57@gmail.com>
2023-01-11 09:05:39 -0300

    Exercicios feitos em aula

--- src/database.ts
+++ src/database.ts
@@ -1,4 +1,4 @@
-import { COURSE_STACK, TCourse } from "./types"
+import { COURSE_STACK, TCourse, TStudent } from "./types"
 
 export const courses: TCourse[] = [
     {
@@ -19,4 +19,18 @@ export const courses: TCourse[] = [
         lessons: 5,
         stack: COURSE_STACK.BACK
     }
+]
+
+export const students: TStudent[] = [
+    {
+        id: "c001",
+        name: "Arthur",
+        age: 20
+    },
+    {
+        id: "c002",
+        name: "Bruno",
+        age: 25
+    }
+
 ]
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import express, { Request, Response } from 'express'
+ import express, { Request, Response } from 'express'
 import cors from 'cors'
+import { courses, students } from './database'
+import { COURSE_STACK, TCourse } from './types'
 
 const app = express()
 
@@ -12,4 +14,67 @@ app.listen(3003, () => {
 
 app.get('/ping', (req: Request, res: Response) => {
     res.send('Pong!')
+})
+
+app.get('/courses', (req: Request, res: Response ) => 
+{
+    res.status(200).send(courses)
+})
+
+app.get('/courses/search', (req: Request, res: Response) =>{
+    const q = req.query.q as string
+
+    const result = courses.filter((course)=>{
+        return course.name.toLowerCase().includes(q.toLowerCase()) 
+    })
+    res.status(200).send(result)
+})
+
+app.post('/courses', (req: Request, res: Response)=>{
+    const id = req.body.id as string
+    const name = req.body.name as string
+    const lessons = req.body.lessons as number
+    const stack = req.body.stack as COURSE_STACK
+
+    const NewCourse = {
+        id,
+        name,
+        lessons,
+        stack
+    }
+
+    courses.push(NewCourse)
+
+    res.status(201).send("Curso registrado com sucesso!")
+})
+
+app.get('/students',(req: Request, res: Response)=>{
+    res.status(200).send(students)
+})
+
+app.get('/students/search', (req: Request, res: Response) =>{
+    const q = req.query.q as string
+
+    const buscaEstudante = students.filter((student)=>{
+        return student.name.toLowerCase().includes(q.toLowerCase())
+    })
+
+    res.status(200).send(buscaEstudante)
+})
+
+app.post('/students', (req: Request, res: Response)=>{
+    const id = req.body.id as string
+    const name = req.body.name as string
+    const age = req.body.age as number
+    
+
+    const NewStudent = {
+        id,
+        name,
+        age
+    }
+
+    students.push(NewStudent)
+
+    res.status(201).send("Estudante registrado com sucesso!")
 })
